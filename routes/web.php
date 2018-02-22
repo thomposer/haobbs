094c627c74e5f74c5060b601e168a8038774ed6c
CollierLiu <luxken@qq.com>
2018-02-22 08:05:41 +0000

    User can upload topic image

--- routes/web.php
+++ routes/web.php
@@ -34,3 +34,6 @@ Route::get('/users/{user}', 'UsersController@show')->name('users.show');
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
 
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
+
+// 话题图片上传
+Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
