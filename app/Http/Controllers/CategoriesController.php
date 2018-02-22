ab0a736a4acb8020862cb29ebb53547e78f3ef2a
CollierLiu <luxken@qq.com>
2018-02-22 05:57:16 +0000

    Topic order

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -8,10 +8,10 @@ use App\Models\Category;
 
 class CategoriesController extends Controller
 {
-    public function show(Category $category)
+    public function show(Category $category, Request $request, Topic $topic)
     {
         // 读取与分类 ID 相关的话题，并按每页 20 条分页
-        $topics = Topic::with('user', 'category')->where('category_id', $category->id)->paginate(20);
+        $topics = $topic->withOrder($request->order)->where('category_id', $category->id)->paginate(20);
 
         return view('topics.index', compact('topics', 'category'));
     }
