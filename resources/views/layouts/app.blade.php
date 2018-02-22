0bea385427c96d4cd0e3d6eb9835dc472b2ae99d
CollierLiu <luxken@qq.com>
2018-02-22 07:34:17 +0000

    WYSIWYG Editor

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'LaraBBS') - Laravel 进价</title>
     <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
+    @yield('styles')
 </head>
 <body>
     <div id="app" class="{{ route_class() }}-page">
@@ -18,5 +19,6 @@
         @include('layouts._footer')
     </div>
     <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
+    @yield('scripts')
 </body>
 </html>
--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -1,24 +1,40 @@
 @extends('layouts.app')
 
+@section('styles')
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/simditor.css') }}">
+@stop
+
+@section('scripts')
+    <script type="text/javascript" src="{{ asset('js/module.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/hotkeys.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/uploader.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/simditor.js') }}"></script>
+    <script type="text/javascript">
+    $(document).ready(function() {
+        var editor = new Simditor({
+            textarea: $('#editor')
+        });
+    });
+    </script>
+@stop
+
 @section('content')
 
 <div class="container">
     <div class="col-md-10 col-md-offset-1">
         <div class="panel panel-default">
-            <div class="panel-heading">
-                <h1>
-                    <i class="glyphicon glyphicon-edit"></i> Topic /
+            <div class="panel-body">
+                <h2 class="text-center">
+                    <i class="glyphicon glyphicon-edit"></i>
                     @if($topic->id)
                         编辑话题
                     @else
-                        创建话题
+                        新建话题
                     @endif
-                </h1>
-            </div>
+                </h2>
 
-            @include('common.error')
+                @include('common.error')
 
-            <div class="panel-body">
                 @if($topic->id)
                     <form action="{{ route('topics.update', $topic->id) }}" method="POST" accept-charset="UTF-8">
                         <input type="hidden" name="_method" value="PUT">
@@ -38,7 +54,7 @@
                         </select>
                     </div>
                     <div class="form-group">
-                    	<textarea name="body" id="body-field" class="form-control" rows="3" placeholder="请填入至少三个字符的内容。">{{ old('body', $topic->body ) }}</textarea>
+                    	<textarea name="body" id="editor" class="form-control" rows="3" placeholder="请填入至少三个字符的内容。">{{ old('body', $topic->body ) }}</textarea>
                     </div>
                     <div class="well well-sm">
                         <button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-ok" aria-hidden="true"></span> 保存</button>
