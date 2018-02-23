d16f565ca5b337c7e1d2f4031ec8397e71da3e87
CollierLiu <luxken@qq.com>
2018-02-23 02:32:21 +0000

    Admin can manage users

--- app/Models/User.php
+++ app/Models/User.php
@@ -63,4 +63,25 @@ class User extends Authenticatable
         $this->save();
         $this->unreadNotifications->markAsRead();
     }
+
+    public function setPasswordAttribute($value)
+    {
+        // 如果值的长度等于 60，即认为是已经做过加密的情况
+        if (strlen($value) != 60) {
+            // 不等于 60，做密码加密处理
+            $value = bcrypt($value);
+        }
+
+        $this->attributes['password'] = $value;
+    }
+
+    public function setAvatarAttribute($path)
+    {
+        // 如果不是 http 开头的则补全
+        if ( ! starts_with($path, 'http')) {
+            $path = config('app.url') . "/uploads/images/avatars/$path";
+        }
+
+        $this->attributes['avatar'] = $path;
+    }
 }
