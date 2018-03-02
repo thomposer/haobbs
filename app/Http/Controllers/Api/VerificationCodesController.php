a7bae32551a60fcee0a3a91787552aa9e9538b43
Collier Liu <luxken@qq.com>
2018-03-02 08:18:18 +0000

    add captcha

--- app/Http/Controllers/Api/VerificationCodesController.php
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -9,25 +9,43 @@ class VerificationCodesController extends Controller
 {
     public function store(VerificationCodeRequest $request, EasySms $easySms)
     {
-        $phone = $request->mobile;
+        // 判断图形验证码
+        $captchaData = \Cache::get($request->captcha_key);
+        if (!$captchaData) {
+            return $this->response->error('图形验证码已失效', 422);
+        }
 
-        // 生成4位随机数，左侧补0
-        $code = str_pad(random_int(1, 9999), 4, 0, STR_PAD_LEFT);
+        if (!hash_equals($captchaData['code'], $request->captcha_code)) {
+            // 验证错误就清除缓存
+            \Cache::forget($request->captcha_key);
+            return $this->response->errorUnauthorized('验证码错误');
+        }
 
-        try {
-            $result = $easySms->send($phone, [
-                'content'  =>  "【BBS社区】您的验证码是{$code}"
-            ]);
-        } catch (\GuzzleHttp\Exception\ClientException $exception) {
-            $response = $exception->getResponse();
-            $result = json_decode($response->getBody()->getContents(), true);
-            return $this->response->errorInternal($result['msg'] ?? '短信发送异常');
+        $phone = $captchaData['mobile'];
+
+        if (!app()->environment('production')) {
+            $code = '1234';
+        } else {
+            // 生成4位随机数，左侧补0
+            $code = str_pad(random_int(1, 9999), 4, 0, STR_PAD_LEFT);
+
+            try {
+                $result = $easySms->send($phone, [
+                    'content' => "【BBS社区】您的验证码是{$code}"
+                ]);
+            } catch (\GuzzleHttp\Exception\ClientException $exception) {
+                $response = $exception->getResponse();
+                $result = json_decode($response->getBody()->getContents(), true);
+                return $this->response->errorInternal($result['msg'] ?? '短信发送异常');
+            }
         }
 
         $key = 'verificationCode_'.str_random(15);
         $expiredAt = now()->addMinutes(10);
         // 缓存验证码 10分钟过期。
         \Cache::put($key, ['mobile' => $phone, 'code' => $code], $expiredAt);
+        // 清除图形验证码缓存
+        \Cache::forget($request->captcha_key);
 
         return $this->response->array([
             'key' => $key,
